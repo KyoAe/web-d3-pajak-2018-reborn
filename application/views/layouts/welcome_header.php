8d8cf9734646f501e774f25e4114ed6b932d8238
Giovanni Octa Anggoman <darkcoder@pop-os.localdomain>
2020-11-21 20:13:35 +0800

    Update page title
    Update page title to D III Pajak 2018

--- application/views/layouts/welcome_header.php
+++ application/views/layouts/welcome_header.php
@@ -11,11 +11,11 @@
 	<meta name="robots" content="" />
 	
 	<!-- DESCRIPTION -->
-	<meta name="description" content="<?= $title ?> - Website D3 Pajak 2018" />
+	<meta name="description" content="<?= $title ?> - D III Pajak 2018" />
 	
 	<!-- OG -->
-	<meta property="og:title" content="<?= $title ?> - Website D3 Pajak 2018" />
-	<meta property="og:description" content="<?= $title ?> - Website D3 Pajak 2018" />
+	<meta property="og:title" content="<?= $title ?> - D III Pajak 2018" />
+	<meta property="og:description" content="<?= $title ?> - D III Pajak 2018" />
 	<meta property="og:image" content="" />
 	<meta name="format-detection" content="telephone=no">
 	
@@ -23,7 +23,7 @@
 	<link rel="icon" href="<?=base_url(); ?>public/images/favicon.ico" type="image/x-icon" />
 	<link rel="shortcut icon" type="image/x-icon" href="<?=base_url(); ?>public/images/favicon.png" />
 	
-	<title><?= $title ?> - Website D3 Pajak 2018 </title>
+	<title><?= $title ?> - D III Pajak 2018 </title>
 	
 	<!-- MOBILE SPECIFIC ============================================= -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
