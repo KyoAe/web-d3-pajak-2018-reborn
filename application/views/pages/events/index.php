8bf9b07f88d92659df56ea454f16217c9750b3f6
kyoae <giovannianggoman1@gmail.com>
2021-01-09 16:02:42 +0800

    Added Kalender Angkatan

--- application/views/pages/events/index.php
+++ application/views/pages/events/index.php
@@ -25,11 +25,11 @@
     <div class="content-block">
         <!-- Portfolio  -->
         <div class="section-area section-sp1 gallery-bx">
-            <div class="container text-center">
-                <!-- <p>Untuk menambahkan kalender ini ke kalender Anda, tekan tombol "Add to Google Calendar" yang ada di bawah kanan</p> -->
+            <div class="container">
+                <p>Untuk menambahkan kalender ini ke kalender Anda, tekan tombol <img src="https://drive.google.com/uc?id=1OpvB9hKLVt_Bp0TDXXe71sv0TITJquzG" alt="+GoogleKalender"></span> yang ada di kanan bawah.</p>
                 <!-- Embed Google Calendar untuk web angkatan tidak resmi. Kalender milik Gio -->
                 <div class="mx-auto">
-                    <h2>COMING SOON</h2>
+                    <iframe src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FJakarta&amp;showPrint=0&amp;showTabs=1&amp;title=Kalender%20D%20III%20Pajak%202018&amp;hl=id&amp;src=dGF4ZXIyazE4cGtuc3RhbkBnbWFpbC5jb20&amp;color=%23039BE5" style="border-width:0" width="100%" height="800" frameborder="0" scrolling="no"></iframe>
                 </div>                
             </div>
         </div>
