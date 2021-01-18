84816481e50189dbd1626085a8ada94ba1c958d7
kyoae <giovannianggoman1@gmail.com>
2021-01-18 15:27:45 +0800

    Used loop and branching in study_report view

--- application/views/pages/dashboard/study_report.php
+++ application/views/pages/dashboard/study_report.php
@@ -30,84 +30,47 @@
               </div>
               <div class="card-body">
                 <div class="tab-content" id="custom-tabs-one-tabContent">
-                  <div class="tab-pane fade show active" id="semester1" role="tabpanel" aria-labelledby="semester1-tab">
+                  <?php for($semester=1; $semester<=6; $semester++): ?>
+                  <div class="tab-pane fade show <?= ($semester == 1) ? "active" : ""?>" id="semester<?= $semester ?>" role="tabpanel" aria-labelledby="semester<?=$semester?>-tab">
+                    <?php if (empty($grades[$semester])): ?>
+                      <h1>NILAINYA KUMPULIN GAN!</h1>
+                    <?php else: ?>
                     <table class="table table-striped">
-                        <thead>
-                            <tr>
-                            <th scope="col">Nama Mata Kuliah</th>
-                            <th scope="col">SKS</th>
-                            <th scope="col">Angka</th>
-                            <th scope="col">Huruf</th>
-                            <th scope="col">Indeks</th>
-                            </tr>
-                        </thead>
-                            <tbody>
-                                <tr>
-                                    <?php 
-                                        foreach($grades[1] as $matkul)
-                                        {
-                                            echo "<tr>";
-                                            echo "<th> $matkul->name </th>";
-                                            echo "<th> $matkul->credits </th>";
-                                            echo "<th> $matkul->percentage </th>";
-                                            echo "<th> $matkul->letter </th>";
-                                            echo "<th>" .($matkul->fp_scale). "</th>";
-                                            echo "</tr>";
-                                        }
-                                    ?>
-                                </tr>
-                            </tbody>
+                      <thead>
+                        <tr>
+                        <th scope="col">Nama Mata Kuliah</th>
+                        <th scope="col">SKS</th>
+                        <th scope="col">Angka</th>
+                        <th scope="col">Huruf</th>
+                        <th scope="col">Indeks</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <?php 
+                            $total_credits = 0;
+                            foreach($grades[$semester] as $matkul)
+                            {
+                              $total_credits += $matkul->credits;
+                              echo "<tr>";
+                              echo "<th> $matkul->name </th>";
+                              echo "<th> $matkul->credits </th>";
+                              echo "<th> $matkul->percentage </th>";
+                              echo "<th> $matkul->letter </th>";
+                              echo "<th>" .($matkul->fp_scale). "</th>";
+                              echo "</tr>";
+                            }
+                          ?>
+                        </tr>
+                      </tbody>
                     </table>
-                    <h6 style="text-align:right; font-weight:bold">Total SKS : 20</h6>
-                    <h6 style="text-align:right; font-weight:bold">IPK Semester : <?php echo($gpas[1]) ?></h6>
-                    <h6 style="text-align:right; font-weight:bold">Rangking <?php echo($ranks[1]) ?> dari <?php echo($student_counts[1]) ?> mahasiswa</h6>
+                    <h6 style="text-align:right; font-weight:bold">Total SKS : <?= $total_credits ?></h6>
+                    <h6 style="text-align:right; font-weight:bold">IPK Semester : <?php echo($gpas[$semester]) ?></h6>
+                    <h6 style="text-align:right; font-weight:bold">Rangking <?php echo($ranks[$semester]) ?> dari <?php echo($student_counts[$semester]) ?> mahasiswa</h6>
                     <h6 style="margin-top: 50px; font-weight:bold"><b>NB : Dalam hal terjadi IPK sama, peringkat diurutkan berdasarkan abjad</b></h6>
+                    <?php endif; ?>
                   </div>
-                  <div class="tab-pane fade" id="semester2" role="tabpanel" aria-labelledby="semester2-tab">
-                  <table class="table table-striped">
-                        <thead>
-                            <tr>
-                            <th scope="col">Nama Mata Kuliah</th>
-                            <th scope="col">SKS</th>
-                            <th scope="col">Angka</th>
-                            <th scope="col">Huruf</th>
-                            <th scope="col">Indeks</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                                <tr>
-                                    <?php 
-                                        foreach($grades[2] as $matkul)
-                                        {
-                                            echo "<tr>";
-                                            echo "<th> $matkul->name </th>";
-                                            echo "<th> $matkul->credits </th>";
-                                            echo "<th> $matkul->percentage </th>";
-                                            echo "<th> $matkul->letter </th>";
-                                            echo "<th>" .($matkul->fp_scale). "</th>";
-                                            echo "</tr>";
-                                        }
-                                    ?>
-                                </tr>
-                            </tbody>
-                    </table>
-                    <h6 style="text-align:right; font-weight:bold">Total SKS : 21</h6>
-                    <h6 style="text-align:right; font-weight:bold">IPK Semester : <?php echo($gpas[2]) ?></h6>
-                    <h6 style="text-align:right; font-weight:bold">Rangking <?php echo($ranks[2]) ?> dari <?php echo($student_counts[1]) ?> mahasiswa</h6>
-                    <h6 style="margin-top: 50px; font-weight:bold"><b>NB : Dalam hal terjadi IPK sama, peringkat diurutkan berdasarkan abjad</b></h6>
-                  </div>
-                  <div class="tab-pane fade" id="semester3" role="tabpanel" aria-labelledby="semester3-tab">
-                    <h1>NILAINYA KUMPULIN GAN!</h1>
-                  </div>
-                  <div class="tab-pane fade" id="semester4" role="tabpanel" aria-labelledby="semester4-tab">
-                    <h1>NILAINYA KUMPULIN GAN!</h1>
-                  </div>
-                  <div class="tab-pane fade" id="semester5" role="tabpanel" aria-labelledby="semester5-tab">
-                    <h1>NILAINYA KUMPULIN GAN!</h1>
-                  </div>
-                  <div class="tab-pane fade" id="semester6" role="tabpanel" aria-labelledby="semester6-tab">
-                    <h1>NILAINYA KUMPULIN GAN!</h1>
-                  </div>
+                  <?php endfor; ?>
                   <div class="tab-pane fade" id="rekap" role="tabpanel" aria-labelledby="rekap-tab">
                     <h1>NILAINYA KUMPULIN GAN!</h1>
                   </div>
